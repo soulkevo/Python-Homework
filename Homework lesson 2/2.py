e3a075340b6ad93a4818fb8fb5575f0ca779cc12
Vlad Bachinskiy <bochalife@gmail.com>
2026-07-03 21:13:00 +0300

    Added new lines

---
+++ Homework lesson 2/2.py
@@ -0,0 +1,8 @@
+first_number = int(input("Enter a number: "))
+second_number = int(input("Enter another number: "))
+third_number = int(input("Enter another number: "))
+
+result = (first_number + second_number + third_number) / 3
+
+print(result)
+
