fe05cdd1adc12c84633e770aad3399386a224376
Vlad Bachinskiy <bochalife@gmail.com>
2026-07-16 19:24:00 +0300

    Added new lines

---
+++ Homework lesson 5/5.1.py
@@ -0,0 +1,27 @@
+import keyword, string
+
+name_def =input("Variable name: ")
+is_valid = True
+
+
+for test_case in name_def:
+    if test_case == "_":
+        continue
+    elif test_case in string.punctuation:
+        is_valid = False
+        break
+    else:
+        continue
+
+if name_def in keyword.kwlist:
+    is_valid = False
+elif name_def.islower() == False:
+    is_valid = False
+elif name_def[0].isdigit():
+    is_valid = False
+elif "__" in name_def:
+    is_valid = False
+elif "___" in name_def:
+    is_valid = False
+
+print(f"{is_valid}")
